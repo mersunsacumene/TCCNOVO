3a202d6c46b2bc9f2f209b37742fb88e9a51307c
mersunsacumene <ecomercialmersun@gmail.com>
2024-09-04 21:52:11 -0300

    Configurando CSS

---
+++ TCC-Novo/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: { AzulEscuro: "#024165", Laranja: "#D48325" },
+    },
+  },
+  plugins: [],
+};
